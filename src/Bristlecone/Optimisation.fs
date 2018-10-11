65ea17b3fda86b6d55d697b812a33de4408314b0
Andrew Martin <AndrewIOM@users.noreply.github.com>
2018-10-11 17:01:43 +0100

    Start introducing additional MCMC types with adaptation

--- src/Bristlecone/Optimisation.fs
+++ src/Bristlecone/Optimisation.fs
@@ -5,31 +5,66 @@ type Solution = float * Point
 type Objective = Point -> float
 type Domain = (float*float*Bristlecone.Parameter.Constraint) []
 
+module Distribution =
+
+    open MathNet.Numerics.LinearAlgebra
+
+    let mapply (m:Matrix<float>) f = 
+        m.EnumerateIndexed() |> Seq.iter(fun (i,j,v) -> m.[i,j] <- f v ); m
+
+    module MutlivariateNormal =
+
+        // See: https://stackoverflow.com/questions/11277146/multivariate-normal-distribution-with-math-net
+        let sample' (rnd:System.Random) = 
+            fun () ->
+                let rec randomNormal () = 
+                    let u1, u2 = rnd.NextDouble(),rnd.NextDouble()
+                    let r = sqrt (-2. * (log u1))
+                    let theta = 2. * System.Math.PI * u2  
+                    seq { 
+                        yield r * sin theta
+                        yield r * cos theta 
+                        yield! randomNormal() }
+                randomNormal ()
+
+        let sample (cov:Matrix<float>) rnd = 
+            let R = 
+                if cov.Determinant() = 0. 
+                then
+                    let t = cov.Svd true
+                    let u,d = t.U, t.W
+                    let A = (mapply d sqrt) * u.Transpose()
+                    let qr = A.QR() in qr.R.Transpose()
+                else cov.Cholesky().Factor
+            fun () -> 
+                let v = vector ( sample' rnd () |> Seq.take cov.ColumnCount |> List.ofSeq )
+                R * v
+
+
+
 /// MCMC random walk algorithm.
 /// For more information, see https://doi.org/10.3389/fams.2017.00006.
+/// See also Haario, Saksman and Tamminen 2001: https://projecteuclid.org/download/pdf_1/euclid.bj/1080222083 
 module MonteCarlo =
 
-    let initialise (d:Domain) (rng:System.Random) =
-        [| for (min,max,_) in d -> min + (max-min) * rng.NextDouble() |]
+    // NB Lets set this up so that tuning only occurs during the burnin phase, then stops.
+    // TODO Move all MathNet distribution work into a seperate module
+
+    open MathNet.Numerics.LinearAlgebra
 
     /// Current implementation draws from an independent univariate normal distribution for each individual parameter. 
     let draw random mean stdev =
         let distribution = MathNet.Numerics.Distributions.Normal(mean,stdev,random)
         fun () -> distribution.Sample()
 
-    /// Find an appropriate scale factor for a standard deviation and its acceptance rate.
-    /// [Adapted from PyMC3]
-    let tuneScale scale accRate =
-        match accRate with
-        | a when a > 0.95   -> scale * 10.0
-        | a when a > 0.75   -> scale * 2.0
-        | a when a > 0.50   -> scale * 1.1
-        | a when a < 0.001  -> scale * 0.1
-        | a when a < 0.05   -> scale * 0.5
-        | a when a < 0.20   -> scale * 0.9
-        | _ -> scale
+    let initialise (d:Domain) (rng:System.Random) =
+        [| for (min,max,_) in d -> (draw rng (min + (max-min) / 2.) ((max - min) / 6.))() |]
 
-    let constrainedJump a b (scaleFactor:float) c =
+    // let drawZeroMeanMultivariate random sigma domain =
+    //     let distribution = MathNet.Numerics.Distributions.MatrixNormal(sigma, covarianceMatrix (domain |> Seq.length) 0., sigma, random)
+    //     fun () -> distribution.Sample()
+
+    let constrainJump a b (scaleFactor:float) c =
         match c with
         | Bristlecone.Parameter.Constraint.Unconstrained -> a + (b * scaleFactor)
         | Bristlecone.Parameter.Constraint.PositiveOnly ->
@@ -37,73 +72,142 @@ module MonteCarlo =
                 then (a - (b * scaleFactor)) 
                 else (a + (b * scaleFactor))
 
+    /// f -> a log-likelihood function
+    let rec metropolisHastings' propose tune f theta1 l1 remaining d scale =
 
-    /// NB Requires log-likelihood generating objective
-    let rec metropolis' propose f theta1 l1 remaining d scale =
+        let sc = scale |> tune remaining d
+        let theta2 = theta1 |> propose sc
+        let thetaAccepted, lAccepted = 
+            let l2 = f theta2
+            if l2 < l1
+            then 
+                // printfn "Accepting %A" l2
+                theta2, l2
+            else
+                let rand = MathNet.Numerics.Distributions.ContinuousUniform(0.,1.).Sample()
+                let ratio = - (l2 - l1) |> exp
+                if rand < ratio && l2 <> infinity && l2 <> -infinity && l2 <> nan
+                    then 
+                        theta2, l2
+                    else theta1, l1
 
-        let tuneInterval = 1000
-        let sc =
-            if remaining % tuneInterval = 0 then
-                if d |> Seq.length > tuneInterval then
-                    let acceptance = float ((d |> Seq.take tuneInterval |> Seq.pairwise |> Seq.where(fun (x,y) -> x <> y) |> Seq.length)) / (float tuneInterval)
+        match remaining with 
+        | r when r <= 0 -> d, sc
+        | _ -> 
+            if remaining % 250 = 0 && d |> Seq.length >= 250 then 
+                let ar = float ((d |> Seq.take 250 |> Seq.pairwise |> Seq.where(fun (x,y) -> x <> y) |> Seq.length)) / (float 250)
+                printfn "Optimisation: %i remaining iterations (value %f, AR = %f)" remaining lAccepted ar
+            metropolisHastings' propose tune f thetaAccepted lAccepted (remaining - 1) ((lAccepted,thetaAccepted)::d) sc
+
+    module TuningMode =
+
+        // Tuning   : History -> Remaining Iterations -> TuningSetting
+        // Propose  : TuningSetting -> Theta1 -> Theta2
+
+        /// Find an appropriate scale factor for a standard deviation and its acceptance rate.
+        let tuneScale scale accRate =
+            match accRate with
+            | a when a > 0.95   -> scale * 10.0
+            | a when a > 0.75   -> scale * 2.0
+            | a when a > 0.50   -> scale * 1.1
+            | a when a < 0.001  -> scale * 0.1
+            | a when a < 0.05   -> scale * 0.5
+            | a when a < 0.20   -> scale * 0.9
+            | _ -> scale
+
+        /// Modifies a scale factor depending on 
+        let scaleFactor tuneInterval remainingIterations history scale =
+            if remainingIterations % tuneInterval = 0 then
+                if history |> Seq.length >= tuneInterval then
+                    let acceptance = float ((history |> Seq.take tuneInterval |> Seq.pairwise |> Seq.where(fun (x,y) -> x <> y) |> Seq.length)) / (float tuneInterval)
                     let tuned = tuneScale scale acceptance
-                    printfn "Tuning: %f (AR = %f; -logL = %f)" tuned acceptance l1
+                    printfn "[Tuning] Scale Factor: %f" tuned
                     tuned
                 else scale
             else scale
 
-        let theta2 = theta1 |> propose scale
+        /// Tunes a covariance matrix based on recent samples.
+        let tuneCovariance weight (recentCovariance:Matrix<float>) (oldCovariance:Matrix<float>) =
+            weight * recentCovariance + (1. - weight) * oldCovariance
 
-        let thetaAccepted, lAccepted = 
-            let l2 = f theta2
-            // printfn "Proposal %f" l2
-            if l2 < l1
-            then 
-                printfn "Accepting %f" l2
-                theta2, l2
-            else
-                // TODO Move randomiser somewhere else
-                let rand = (MathNet.Numerics.Distributions.ContinuousUniform(0.,1.)).Sample()
-                let ratio = - (l2 - l1) |> exp
+        /// Generate a covariance matrix
+        let covarianceMatrix parameterCount (scale:float) =
+            let m4 = MathNet.Numerics.LinearAlgebra.DiagonalMatrix.identity<float> parameterCount
+            (m4 * scale) / sqrt (float parameterCount)
 
-                // printfn "Original was %f. New is %f Ratio is %f" l1 l2 ratio
+        /// Calculates the covariance of a given matrix
+        let computeCovariance (matrix:Matrix<float>) =
+            let columnAverages = matrix.ColumnSums() / (float matrix.RowCount)
+            let centredColumns = matrix.EnumerateColumns() |> Seq.zip columnAverages |> Seq.map (fun (col,avg) -> col - avg) |> Seq.toList
+            let centred = MathNet.Numerics.LinearAlgebra.DenseMatrix.ofColumns centredColumns
+            let normalisationFactor = float <| if matrix.RowCount = 1 then 1 else matrix.RowCount - 1
+            centred.TransposeThisAndMultiply(centred) / normalisationFactor
 
-                if rand < ratio && l2 <> infinity && l2 <> -infinity && l2 <> nan
-                    then 
-                        printfn "Accepting %f on backwards jump (previous was %f). The ratio was %f" l2 l1 ratio
-                        theta2, l2
-                    else 
-                        // printfn "Rejecting %f (current is %f" l2 l1
-                        theta1, l1
+        /// Parameters to matrix
+        let samplesToMatrix (samples:float[][]) =
+            samples |> matrix
 
-        match remaining with 
-        | r when r <= 0 -> d
-        | _ -> 
-            if remaining % 1000 = 0 then printfn "Optimisation: %i remaining iterations" remaining
-            metropolis' propose f thetaAccepted lAccepted (remaining - 1) ((lAccepted,thetaAccepted)::d) sc
+        /// The starting covariance matrix for parameters in a multivariate distribution
+        let defaultCovariance n = covarianceMatrix n 2.38
 
-    let randomWalk burn n domain (f:Point->float) : (float * float[]) list =
+        let covarianceFromBounds n (domain:Domain) random =
+            [| 1 .. n |]
+            |> Array.map(fun _ ->
+                domain
+                |> Array.map (fun (low,high,_) -> draw random 0. ((high - low) / 4.) () ) )
+                |> samplesToMatrix
+                |> computeCovariance
 
-        // Given a set of parameters
-        // - when a jump is proposed
-        // - make sure it is valid, reject it, or have it pre-transformed
+
+        /// Tune previously observed covariance based on most recent period
+        let covariance tuneInterval weighting remaining (history:(float*float[]) seq) scale =
+            if remaining % tuneInterval = 0 then
+                if history |> Seq.length >= tuneInterval then
+                    // let ar = float ((history |> Seq.take tuneInterval |> Seq.pairwise |> Seq.where(fun (x,y) -> x <> y) |> Seq.length)) / (float tuneInterval)
+                    // if ar > 0.20 && ar < 0.50 
+                    // then scale
+                    // else 
+                        // There are now enough samples to compare recent versus original covariance matrix,
+                        // and AR requires some kind of tuning.
+                        // Take 500 off the top of history, and, compute covariance off these
+                        // printfn "Tuning covariance."
+                        history
+                        |> Seq.map snd
+                        |> Seq.take tuneInterval
+                        |> matrix
+                        |> computeCovariance
+                        |> tuneCovariance weighting scale
+                else scale
+            else scale
+
+        let dual tuneInterval weighting remaining history tuningFactors =
+            let cov,sc = tuningFactors
+            let tunedSc = scaleFactor tuneInterval remaining history sc
+            let tunedCov = covariance tuneInterval weighting remaining history cov
+            tunedCov, tunedSc
+
+        let scaleOnly tuneInterval remaining history tuningFactors =
+            let cov,sc = tuningFactors
+            let tunedSc = scaleFactor tuneInterval remaining history sc
+            cov, tunedSc
+
+        let none _ _ factors = factors
+
+    let randomWalk nTune n domain (f:Point->float) : (float * float[]) list =
 
         let random = MathNet.Numerics.Random.MersenneTwister(true)
         let theta = initialise domain random
-        let proposeJump scale theta =
-            domain
-            |> Array.map (fun (low,high,con) -> draw random 0. ((high - low) / 4.) (), con )
+        let initialCovariance = TuningMode.covarianceFromBounds 10000 domain random
+        let sample cov = Distribution.MutlivariateNormal.sample cov random
+        let proposeJump (cov:Matrix<float>,scale) (theta:float[]) =
+            sample ((2.38 ** 2.) * cov / (float theta.Length)) <| ()
+            |> Vector.toArray
+            |> Array.map (fun i -> i * scale)
+            |> Array.mapi (fun i x -> x, domain.[i] )
             |> Array.zip theta
-            |> Array.map (fun (thetai,(zi,con)) -> constrainedJump thetai zi scale con)
+            |> Array.map (fun (thetai,(zi,(_,_,con))) -> constrainJump thetai zi scale con)
 
-        let l1 = f theta
-        if l1 = nan then invalidOp "Initial likelihood could not be calculated"
-        printfn "Burn: Starting %i iterations" burn
-        let burnedL,burnedP = 
-            metropolis' proposeJump f theta l1 burn [] 1.
-            |> List.head
-        printfn "Burn: Complete"
-        metropolis' proposeJump f burnedP burnedL n [] 1.
+        metropolisHastings' proposeJump (TuningMode.scaleOnly 1000) f theta (f theta) nTune [] (initialCovariance,1.) |> snd
 
 
 // Nelder Mead implementation
