2437b6aa00fd05564ff9c456d7b75b24c7b69183
AndrewIOM <AndrewIOM@users.noreply.github.com>
2018-12-03 16:47:50 +0000

    Added Akaike weights calculation

--- src/Bristlecone/ModelSelection.fs
+++ src/Bristlecone/ModelSelection.fs
@@ -2,6 +2,8 @@ namespace Bristlecone.ModelSelection
 
 module Akaike =
 
+    open Bristlecone.ModelSystem
+
     ///**Description**
     /// The Akaike information criterion, a standardised index of model fit quality for models that have different numbers of parameters.
     ///**Parameters**
@@ -27,3 +29,28 @@ module Akaike =
         let aic = aic k logLikelihood
         let correction = (2. * ((float k) ** 2.) + 2. * (float k)) / ((float n) - (float k) - 1.)
         aic + correction
+
+    
+    
+    /// **Description**
+    /// Akaike weights for a set of models.
+    /// The weights can be directly interpreted as conditional probabilities for each model.
+    /// 
+    /// **Output Type**
+    ///   * A `seq<EstimationResult * float>` of estimation results paired to their Akaike weights.
+    ///
+    /// **Exceptions**
+    ///   * `ArgumentException` - occurs when there are no observations within an estimation result.
+    let akaikeWeights (models:EstimationResult seq) =
+        let aiccs =
+            models
+            |> Seq.map(fun m ->
+                let n = (m.Series |> Seq.head).Value.Observed.Length
+                aicc n m.Parameters.Count m.Likelihood)
+        let relativeLikelihoods =
+            aiccs
+            |> Seq.map(fun aicc -> exp ( - (1. / 2.) * (aicc - (aiccs |> Seq.min))))
+        let totalLikelihood = relativeLikelihoods |> Seq.sum
+        relativeLikelihoods
+        |> Seq.map (fun relative -> relative / totalLikelihood)
+        |> Seq.zip models
