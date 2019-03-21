075d5ea50ee06ad232879359fcd33f65305cbb7e
AndrewIOM <AndrewIOM@users.noreply.github.com>
2019-03-21 22:19:05 +0000

    Error handling for orchestration agent

--- src/Bristlecone/Workflow.fs
+++ src/Bristlecone/Workflow.fs
@@ -11,17 +11,16 @@ module Orchestration =
         | StartWorkPackage of Async<EstimationResult>
         | StartDependentWorkPackages of Async<EstimationResult>
         | Finished of EstimationResult
-        // | PopResult of EstimationResult option
+        | WorkFailed of exn
+        | WorkCancelled
 
     let print writeOut s =
         s |> WorkloadEvent |> writeOut
 
-    let printError (e:exn) =
-        printfn "Error! %s" e.Message
-
-    type OrchestrationAgent(writeOut, maxSimultaneous) = 
+    type OrchestrationAgent(writeOut, maxSimultaneous, retainResults) = 
 
         let queue = Queue<_>()
+        let results = Queue<_>()
 
         let agent = MailboxProcessor.Start(fun inbox ->
             let rec messageLoop inProgress = async {
@@ -32,29 +31,44 @@ module Orchestration =
                     match msg with
                     | StartWorkPackage work -> 
                         queue.Enqueue(work)
-                        print writeOut "Queued a work parcel"
+                        print writeOut <| sprintf "Work parcel added to queue (%i items are waiting)" queue.Count
                         inProgress
                     | Finished r -> 
-                        print writeOut "A work parcel has finished"
+                        print writeOut <| sprintf "Work parcel has finished (%i items are waiting)" queue.Count
+                        if retainResults then results.Enqueue r
                         inProgress - 1
                     | StartDependentWorkPackages work -> invalidOp "Not implemented"
+                    | WorkFailed e ->
+                        print writeOut <| sprintf "There was an issue completing one work item: %s" e.Message
+                        inProgress - 1
+                    | WorkCancelled ->
+                        print writeOut "A work parcel was cancelled"
+                        inProgress - 1
 
                 let postProcess =
                     if preProcess < maxSimultaneous && queue.Count > 0
                     then
                         let work = queue.Dequeue()
-                        Async.Start(async {
-                            print writeOut "Started processing a work parcel"
-                            let! r = work
-                            inbox.Post(Finished r) })
+                        Async.Start(
+                            async {
+                                print writeOut "Started processing a work parcel"
+                                try
+                                    let! r = work
+                                    Finished r |> inbox.Post
+                                with error -> inbox.Post <| WorkFailed error })
                         preProcess + 1
                     else preProcess
 
-                print writeOut <| sprintf "There are currently %i running work parcels (max = %i)" inProgress maxSimultaneous
+                print writeOut <| sprintf "Running %i work parcels in parallel (limit = %i)" inProgress maxSimultaneous
                 return! messageLoop postProcess }
 
             messageLoop 0 )
 
-        do agent.Error.Add printError
+        do agent.Error.Add (fun e -> print writeOut <| sprintf "An error occurred in the orchestrator: %s" e.Message)
 
         member __.Post msg = agent.Post msg
+
+        member __.TryGetResult () =
+            if results.Count > 0
+            then results.Dequeue() |> Some
+            else None
