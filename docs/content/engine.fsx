f2ecd98c96b4a766a7d1c8e6887bc245b3eca0da
Andrew Martin <AndrewIOM@users.noreply.github.com>
2019-03-24 21:19:48 +0000

    Additional documentation: optimisation

---
+++ docs/content/engine.fsx
@@ -0,0 +1,11 @@
+(*** hide ***)
+#I "../../bin"
+#I "../../bin/Bristlecone/net47"
+
+(**
+Estimation Engine
+========================
+
+Forthcoming
+
+*)
---
+++ docs/content/model.fsx
@@ -0,0 +1,11 @@
+(*** hide ***)
+#I "../../bin"
+#I "../../bin/Bristlecone/net47"
+
+(**
+Defining a Model System
+========================
+
+Forthcoming
+
+*)
