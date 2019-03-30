3677934ae2e5b178289b38ef1b84f0c84aa29116
Andrew Martin <AndrewIOM@users.noreply.github.com>
2019-03-30 18:29:40 +0000

    Ability to load previously-saved data sheets

--- src/Bristlecone/DataAccess.fs
+++ src/Bristlecone/DataAccess.fs
@@ -28,10 +28,33 @@ module Config =
             sprintf "%sbristlecone-%s-%i-%s-%s.csv" path.FullName subject modelId t (resultId.ToString())
         else invalidArg "directory" "The specified directory does not exist"
 
+    let regexGuid = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"
+
+    /// Find files in a directory that match the standard bristlecone
+    /// file naming scheme.
+    let fileMatch directory subject modelId dataType =
+        let path = System.IO.DirectoryInfo(directory)
+        if path.Exists then
+            let t = 
+                match dataType with
+                | MLE -> "mle"
+                | Trace -> "trace"
+                | Series -> "series"
+            let files = path.GetFiles(sprintf "bristlecone-%s-%i-%s-*.csv" subject modelId t)
+            let regex = sprintf "bristlecone-%s-%i-%s-%s.csv" subject modelId t regexGuid
+            files |> Seq.choose(fun f -> 
+                let m = System.Text.RegularExpressions.Regex.Match(f.Name, regex)
+                if m.Success
+                then 
+                    let guid = m.Captures.[0].Value |> System.Guid.Parse
+                    (guid, f) |> Some
+                else None )
+        else invalidArg "directory" "The specified directory does not exist"
+
 
 module Trace =
 
-    type BristleconeTrace = CsvProvider<"templates/individual-trace.csv">
+    type BristleconeTrace = CsvProvider<"templates/individual-trace.csv", IgnoreErrors = true>
 
     module Row =
 
@@ -52,6 +75,13 @@ module Trace =
             |> Seq.everyNth thinBy
             |> Seq.concat
 
+        let toTrace (data:BristleconeTrace) : (float * float []) list =
+            data.Rows
+            |> Seq.groupBy(fun r -> r.Iteration)
+            |> Seq.map(fun (i,r) -> 
+                (r |> Seq.head).NegativeLogLikelihood, 
+                r |> Seq.map(fun r -> r.ParameterValue) |> Seq.toArray)
+            |> Seq.toList
 
     let save directory subject modelId thinBy result =
         let csv = new BristleconeTrace(result |> Row.fromEstimate thinBy subject modelId)
@@ -60,12 +90,18 @@ module Trace =
 
     /// Load a trace from a file
     let load directory subject modelId =
-        invalidOp "Not implemented"
+        let traceFiles = Config.fileMatch directory subject modelId Config.DataType.Trace
+        traceFiles
+        |> Seq.choose(fun (i,f) ->
+            let data = BristleconeTrace.Load f.FullName
+            match data.Rows |> Seq.length with
+            | 0 -> None
+            | _ -> (i, data |> Row.toTrace) |> Some )
 
 
 module MLE =
 
-    type IndividualMLE = CsvProvider<"templates/individual-mle.csv">
+    type IndividualMLE = CsvProvider<"templates/individual-mle.csv", IgnoreErrors = true>
 
     module Row =
 
@@ -80,6 +116,16 @@ module MLE =
                  result.Likelihood,
                  v |> Parameter.getEstimate) |> IndividualMLE.Row )
 
+        let toResult (data:IndividualMLE) =
+            if data.Rows |> Seq.isEmpty
+            then Error "An MLE file is corrupt"
+            else 
+                let mle = (data.Rows |> Seq.head).NegativeLogLikelihood
+                let pool = 
+                    data.Rows
+                    |> Seq.map(fun r -> (ShortCode.create r.ParameterCode, r.ParameterValue))
+                    |> Map.ofSeq
+                (mle, pool) |> Ok
 
     let save directory subject modelId result =
         let csv = new IndividualMLE (result |> Row.fromResult subject modelId)
@@ -87,12 +133,22 @@ module MLE =
         csv.Save(filePath)
 
     let load directory subject modelId =
-        invalidOp "Not finished"
+        let traceFiles = Config.fileMatch directory subject modelId Config.DataType.MLE
+        traceFiles
+        |> Seq.choose(fun (i,f) ->
+            let data = IndividualMLE.Load f.FullName
+            match data.Rows |> Seq.length with
+            | 0 -> None
+            | _ ->
+                let parsed = data |> Row.toResult
+                match parsed with
+                | Ok mle -> (i, mle) |> Some
+                | Error _ -> None )
 
 
 module Series =
 
-    type IndividualSeries = CsvProvider<"templates/individual-series.csv">
+    type IndividualSeries = CsvProvider<"templates/individual-series.csv", IgnoreErrors = true>
 
     module Row =
 
@@ -104,14 +160,30 @@ module Series =
                 |> TimeSeries.toObservations
                 |> Seq.map(fun (v,t) -> IndividualSeries.Row (subject, hypothesisId, name.Value, t, v.Fit, v.Obs, result.Likelihood )))
 
+        let toSeries (data:IndividualSeries) : CodedMap<FitSeries> =
+            data.Rows
+            |> Seq.groupBy(fun r -> r.Variable)
+            |> Seq.map(fun (g,r) -> 
+                let ts = 
+                    r 
+                    |> Seq.map(fun r -> ({ Fit = r.Expected; Obs = r.Observed}, r.Time))
+                    |> TimeSeries.fromObservations
+                (ShortCode.create g, ts))
+            |> Map.ofSeq
 
     let save directory subject modelId result =
         let csv = new IndividualSeries(result |> Row.fromResult subject modelId)
         let filePath = Config.filePath directory subject modelId result.ResultId Config.DataType.Series
         csv.Save(filePath)
 
-    let load x =
-        invalidOp "Not finished"
+    let load directory subject modelId =
+        let traceFiles = Config.fileMatch directory subject modelId Config.DataType.Trace
+        traceFiles
+        |> Seq.choose(fun (i,f) ->
+            let data = IndividualSeries.Load f.FullName
+            match data.Rows |> Seq.length with
+            | 0 -> None
+            | _ -> (i, data |> Row.toSeries) |> Some )
 
 
 module EstimationResult =
@@ -123,8 +195,23 @@ module EstimationResult =
         MLE.save directory subject modelId result
         Series.save directory subject modelId result
 
-    let loadAll directory subject modelId =
-        invalidOp "Not implemented"
+    /// Load an `EstimationResult` that has previously been saved as
+    /// three seperate dataframes. Results will only be reconstructed
+    /// when file names and formats are in original Bristlecone format.
+    let loadAll directory subject (modelSystem:ModelSystem) modelId =
+        let mles = MLE.load directory subject modelId |> Seq.map(fun (k,v) -> k.ToString(), v)
+        let series = Series.load directory subject modelId |> Seq.map(fun (k,v) -> k.ToString(), v)
+        let traces = Trace.load directory subject modelId |> Seq.map(fun (k,v) -> k.ToString(), v)
+        mles
+        |> Seq.keyMatch series
+        |> Seq.map(fun (k,v1,v2) -> (k, (v1, v2)))
+        |> Seq.keyMatch traces
+        |> Seq.map(fun (k,t,(s,(l,p))) ->
+            { ResultId = k |> System.Guid.Parse
+              Likelihood = l
+              Parameters = modelSystem.Parameters |> Map.map(fun k v -> Parameter.setEstimate v (p |> Map.find k))
+              Series = s 
+              Trace = t })
 
 
 module ModelSelection =
