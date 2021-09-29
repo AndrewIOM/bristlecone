c2348ca3ed4abdf6487d498d55bbe71a8e31edac
AndrewIOM <AndrewIOM@users.noreply.github.com>
2021-09-29 16:25:44 +0100

    Added docs for language + model system

---
+++ docs/language.fsx
@@ -0,0 +1,139 @@
+(**
+---
+title: Writing Models
+category: The Bristlecone Language
+categoryindex: 1
+index: 2
+---
+*)
+
+(*** condition: prepare ***)
+#nowarn "211"
+#r "../src/Bristlecone/bin/Release/netstandard2.0/Bristlecone.dll"
+(*** condition: fsx ***)
+#if FSX
+#r "nuget: Bristlecone,{{package-version}}"
+#endif // FSX
+(*** condition: ipynb ***)
+#if IPYNB
+#r "nuget: Bristlecone,{{package-version}}"
+#endif // IPYNB
+
+(**
+The Bristlecone Language: Writing Models
+======================
+
+Bristlecone includes a language for expressing mathematical models, and combining
+these to form hypotheses for testing. You can use the language by opening it's
+namespace:
+
+*)
+
+open Bristlecone            // Opens Bristlecone core library and estimation engine
+open Bristlecone.Language   // Open the language for writing Bristlecone models
+
+(**
+
+Writing mathematical expressions
+---
+
+In Bristlecone, mathematical expressions to be used within
+a model may be written using simple terms. Consider the following
+example of an ordinary differential equation of logistic growth:
+*)
+
+let dNdt = Parameter "r" * This * (Constant 1. - (This / Parameter "K"))
+
+(** 
+Here, we have used the following model expressions:
+
+  * `This` - the subject of the equation, which in this 
+    case is *N*, the population size.
+
+  * `Parameter` - an estimatable parameter followed by its name. Here, we have
+    defined two parameters:
+      - `Paramater "r"` - the intrinsic growth rate of the population;
+      - `Parameter "K"` - the *carrying capacity* of the population.
+
+  * `Constant` - a fixed parameter or constant value within an equation.
+
+You can use all standard mathematical operators `* / + - % **` to build your model.
+There are additional model expressions that may be used to build up more complex
+expressions (which are defined in the `ModelExpression` discriminated union).
+
+Additional model expressions
+---
+
+### Time-varying parameters
+
+Often, a time-series model needs to take account of environmental
+variability. For example, changes in temperature and precipitation
+may influence the growth rate of a plant over time. Bristlecone supports
+time-varying parameters in equations using the form:
+*)
+
+Environment "T[min]"
+
+(**
+Here, we have defined the minimum temperature (*T[min]*) as a time-varying (or environment) parameter.
+Unlike `Paramater` items, *T[min]* will not be estimated during model-fitting, but
+will be read from the environmental properties for the appropriate time interval.
+
+### Conditional
+
+You can define a conditional model fragment by using the
+`Conditional` term. A `Conditional` model expression takes a function
+with one argument - a function that represents the future
+computation of the model expression (`compute` -
+with signature `ModelExpression -> float`). Within this function,
+you can then evaluate the answer of any model expressions
+by passing them as the argument to the `compute` function.
+Please see the below example:
+*)
+
+let linear =
+    Conditional(fun compute ->
+        if compute (Parameter "a" * Constant 5.) < 1e-12 
+        then Invalid 
+        else Parameter "a" * This)
+
+(**
+In this example, the function is limited such that
+the combination of parameter value *a* with a theoretical
+value of *This* at 5.0 cannot result in an answer that is 
+less than 1e-12. If this is the case, the result of the 
+model is `Invalid`.
+
+### Invalid
+
+The `Invalid` keyword is useful in combination with the
+`Conditional` keyword, as demonstrated above. When any model expression
+returns `Invalid`, the model fails. During optimisation, this is useful
+to constrain the search within parameter space to values that do not
+tend to infinity, or where values may be theoretically implausable.
+
+### Arbitrary
+
+Sometimes, a complex sub-model may need to be inserted where you
+do not wish to rewrite it into the Bristlecone language. You may also
+not have access to the original source, or be calling from an external
+library. In these cases, you can use the `Arbitrary` model expression
+to wrap the function for Bristlecone. Helper functions are included
+to simplify this process within the `Arbitrary` module:
+*)
+
+/// x: population size
+/// t: current time
+/// z: some environmental value
+let someArbitraryFn x t z =
+    x + t * 1.0 - z
+
+// TODO Insert example here
+
+(**
+
+### Time
+
+The `Time` keyword simply retrives the current time index; as such, it
+can be used to formulate time-dependent models.
+*)
--- docs/model.fsx
+++ docs/model.fsx
@@ -1,9 +1,9 @@
 (**
 ---
-title: Defining a model system
-category: Getting Started
+title: Defining a Hypothesis (or Model System)
+category: The Bristlecone Language
 categoryindex: 1
-index: 1
+index: 2
 ---
 *)
 
@@ -20,9 +20,84 @@ index: 1
 #endif // IPYNB
 
 (**
-Defining a Model System
+Defining a Hypothesis (or Model System)
 ========================
 
-Forthcoming
+In Bristlecone, a `ModelSystem` represents combination
+of mathematical models and their estimatable parameters
+that can be used within model-fitting and model-selection.
+A `ModelSystem` is defined by:
+
+* at least one equation;
+* at least one parameter to estimate;
+* a likelihood function; and
+* optionally, measurement equations.
+
+To build a `ModelSystem`, first open the Bristlecone language:
+*)
+
+open Bristlecone            // Opens Bristlecone core library and estimation engine
+open Bristlecone.Language   // Open the language for writing Bristlecone models
+
+(**
+Second, define the parts of your hypothesis. In this example,
+we will define a simple model of population growth:
+
+*Note: refer to the section on [writing models](language.html) for help writing
+Bristlecone expressions.*
+*)
+
+let ``dN/dt`` = Parameter "r" * This * (Constant 1. - (This / Parameter "K"))
+
+(** 
+Our model of the change in *N* (population count) over time has two estimatable parameters defined using the `Parameter` term,
+which are *r* (the intrinsic growth rate of the population)
+and *K* (the carrying capacity of the population). 
+
+Next, we need to create our `ModelSystem` by using the helper functions
+that can be found under `Model.*`.
+
+In this example, we will use a sum-of-squares measure to determine
+the goodness-of-fit rather than a likelihood function. A selection of
+likelihood functions, including sum-of-squares functions, are included
+in Bristlecone within the `ModelLibrary` module.
+
+The `ModelSystem` can be created with forward pipes (`|>`) as follows:
+*)
+
+let hypothesis =
+    Model.empty
+    |> Model.addEquation       "N"      ``dN/dt``
+    |> Model.estimateParameter "r"      noConstraints 0.10 5.00 
+    |> Model.estimateParameter "K"      noConstraints 50.0 150.
+    |> Model.useLikelihoodFunction      (ModelLibrary.Likelihood.sumOfSquares [ "N" ])
+    |> Model.compile
+
+(**
+In the above code, we first start with an empty model system,
+`Model.empty`. We then add each component with a short code to
+represent it, and finally call `Model.compile`. The `Model.compile`
+function checks the validity of the model in terms of duplicate short codes,
+making sure all parameters are set to be estimated, and that all the
+required parts are in place.
+
+Let's look at defining the estimatable parameters in more detail:
+*)
+
+Model.estimateParameter "r"      noConstraints 0.10 5.00
+
+(**
+For the `Model.estimateParameter` function, first pass 
+the short code as you have used it in your above model expressions.
+Next, you must pass the constraint mode. In Bristlecone, although
+the optimisation algorithms are *unconstrained*, it is possible
+to constrain parameters to specific values using transformations. These
+are applied automatically within the model-fitting process. Two available
+options are `noConstaints`, which is unconstrained, and `positiveOnly`, which
+prevents the value of that parameter dropping below zero. The last 
+arguments are two numbers representing a lower and upper bound
+from which to draw an initial starting value for the parameter.
+The starting value is drawn from a continuous uniform distribution,
+using a `Random` that you define in the `EstimationEngine` (see [estimating](estimation-engine.html)).
 
 *)
